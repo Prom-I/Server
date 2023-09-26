1f2283361be0f1cca91a58f15eed5239d2f3c53e
geniuus <cross_man@naver.com>
2023-09-26 14:45:42 +0900

    파일 업로드

---
+++ Promi/src/main/java/com/example/promi/PromiApplication.java
@@ -0,0 +1,13 @@
+package com.example.promi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromiApplication.class, args);
+    }
+
+}
---
+++ Promi/src/test/java/com/example/promi/PromiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
