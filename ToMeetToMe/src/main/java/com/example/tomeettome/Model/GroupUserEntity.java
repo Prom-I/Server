34a24290eb718c3d0ff66e34b6c473ec59bed15d
geniuus <cross_man@naver.com>
2023-10-24 19:58:12 +0900

    FEAT/ Entity 작성완료

---
+++ ToMeetToMe/src/main/java/com/example/tomeettome/Model/GroupUserEntity.java
@@ -0,0 +1,36 @@
+package com.example.tomeettome.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "GroupUser")
+public class GroupUserEntity {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    private String originKey;
+
+    private String userOriginKey;
+    private String groupOriginKey;
+
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedAt;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
