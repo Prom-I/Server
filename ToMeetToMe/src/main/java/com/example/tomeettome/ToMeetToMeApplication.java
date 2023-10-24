cf7de2cb72e06ee4cfff97d20b3c4abfb10b288a
geniuus <cross_man@naver.com>
2023-10-24 17:12:39 +0900

    BUG/ Build 완료, 실행은 안됨

---
+++ ToMeetToMe/src/main/java/com/example/tomeettome/ToMeetToMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomeettome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToMeetToMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToMeetToMeApplication.class, args);
+    }
+
+}
---
+++ ToMeetToMe/src/test/java/com/example/tomeettome/ToMeetToMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomeettome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToMeetToMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
