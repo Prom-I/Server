f670fb1e2f374601ea734e421562f9773aa55615
geniuus <cross_man@naver.com>
2023-12-08 00:07:03 +0900

    BUG/ preferredDay 반영해서 정상적으로 Parsing 확인

--- ToMeetToMe/src/main/java/com/example/tomeettome/Service/PreferenceService.java
+++ ToMeetToMe/src/main/java/com/example/tomeettome/Service/PreferenceService.java
@@ -46,7 +46,7 @@ public class PreferenceService {
         List<String> times = new ArrayList<>();
         // 블록 개수가 10개면 가능한 블록들을 시간으로 바꿔서 넣으려면 시작시간부터 블록 개수를 하나씩 늘려가며 더해주면 됨
         // 09시 시작이고 duration은 2, 블록 개수 10개면 List에는 ["09", "10", ..]
-        for (int i=0; i<timeRange; i++) {
+        for (int i = 0; i < timeRange; i++) {
             int time = Integer.parseInt(startScope[1].substring(0,2)) + i;
             times.add(String.valueOf(time));
         }
@@ -58,19 +58,19 @@ public class PreferenceService {
 
         // 선호 요일을 제외해서 날짜 Parsing
         // 특정 요일을 제외한 날짜들을 얻기
-        Set<Integer> excludedDays = new HashSet<>();
+        Set<Integer> includedDays = new HashSet<>();
 
         // preferredDays가 String 형태로 들어왔기 때문에 parseInt로 바꿔서 넣어줌 -> 에러처리 해야함!
         for(String day : preferredDays) {
-            excludedDays.add(Integer.parseInt(day));
+            includedDays.add(Integer.parseInt(day));
         }
 
         // LocalDate List로 내가 제외할 날짜를 제외해서 약속을 만들 가능성이 있는 날짜를 parsing
-        List<LocalDate> datesWithoutSpecificDays =
-                getDatesWithoutSpecificDays(startDayScope, excludedDays, dayRange);
+        List<LocalDate> datesWithSpecificDays =
+                getDatesWithSpecificDays(startDayScope, includedDays, dayRange);
 
         // 가능한 총 갯수 : 날짜 갯수 * 하루에 일정을 잡을 수 있는 갯수
-        int availableCounts = datesWithoutSpecificDays.size() * timeRange;
+        int availableCounts = datesWithSpecificDays.size() * timeRange;
 
         // 팀원들 각각 일정을 뽑아야 하니까
         TeamEntity team = teamRepository.findByOriginKey(entity.getTeamOriginKey());
@@ -78,60 +78,24 @@ public class PreferenceService {
 
         for (CalendarPermissionEntity p : permissions) {
             List<ScheduleEntity> schedules = calendarService.retrieveOnlyUser(p.getUserId());
-
         }
 
     }
 
-    private static List<LocalDate> getDatesWithoutSpecificDays(LocalDate startDate, Set<Integer> daysToExclude, int range) {
+    private static List<LocalDate> getDatesWithSpecificDays(LocalDate startDate, Set<Integer> daysToInclude, int range) {
         List<LocalDate> dates = new ArrayList<>();
 
         int daysChecked = 0;
 
         while (daysChecked < range) {
-            if (!daysToExclude.contains(startDate.getDayOfWeek().getValue())) {
+            if (daysToInclude.contains(startDate.getDayOfWeek().getValue())) {
                 dates.add(startDate);
-                daysChecked++;
             }
+            daysChecked++;
 
             startDate = startDate.plusDays(1);
         }
 
         return dates;
     }
-
-
-//    public class DatesWithoutSpecificDays {
-//
-//        public static void main(String[] args) {
-//            String dateString = "20231207";
-//            LocalDate date = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyyMMdd"));
-//
-//            // 특정 요일을 제외한 날짜들을 얻기
-//            Set<Integer> excludedDays = new HashSet<>();
-//            excludedDays.add(1); // 월요일 제외
-//            excludedDays.add(7); // 일요일 제외
-//
-//            List<LocalDate> datesWithoutSpecificDays = getDatesWithoutSpecificDays(date, excludedDays);
-//
-//            // 결과 출력
-//            System.out.println("Original Date: " + date);
-//            System.out.println("Dates without specific days: " + datesWithoutSpecificDays);
-//        }
-//
-//        private static List<LocalDate> getDatesWithoutSpecificDays(LocalDate startDate, Set<Integer> daysToExclude) {
-//            List<LocalDate> dates = new ArrayList<>();
-//
-//            while (daysToExclude.contains(startDate.getDayOfWeek().getValue())) {
-//                startDate = startDate.plusDays(1);
-//            }
-//
-//            while (!daysToExclude.contains(startDate.getDayOfWeek().getValue())) {
-//                dates.add(startDate);
-//                startDate = startDate.plusDays(1);
-//            }
-//
-//            return dates;
-//        }
-//    }
 }
