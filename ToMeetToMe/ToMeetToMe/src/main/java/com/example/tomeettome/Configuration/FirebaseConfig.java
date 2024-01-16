e04dd8742981072fe167d8c0f20986d7d69586d6
geniuus <cross_man@naver.com>
2024-01-16 20:13:36 +0900

    BLD/ 빌드 관련 프로세스 정리

--- ToMeetToMe/ToMeetToMe/src/main/java/com/example/tomeettome/Configuration/FirebaseConfig.java
+++ ToMeetToMe/ToMeetToMe/src/main/java/com/example/tomeettome/Configuration/FirebaseConfig.java
@@ -5,6 +5,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
@@ -16,8 +17,9 @@ import java.util.List;
 @Configuration
 public class FirebaseConfig {
 
+    @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource("tomeettome-4fd3a-firebase-adminsdk-eq40o-f57fec419a.json");
+        ClassPathResource resource = new ClassPathResource("/static/tomeettome-3b470-firebase-adminsdk-zrbg7-29660d4437.json");
         InputStream refreshToken = resource.getInputStream();
 
         FirebaseApp firebaseApp = null;
@@ -35,7 +37,7 @@ public class FirebaseConfig {
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(refreshToken))
                     .build();
-            FirebaseApp.initializeApp(options);
+            firebaseApp = FirebaseApp.initializeApp(options);
         }
         return  FirebaseMessaging.getInstance(firebaseApp);
     }
