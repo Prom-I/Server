fca9beb066c4ef8a09327ec3ae9420ac7963199f
geniuus <cross_man@naver.com>
2024-01-16 17:44:28 +0900

    BLD/ EC2 HealthChecker

---
+++ ToMeetToMe/src/main/java/com/example/tomeettome/Controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.example.tomeettome.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/healthcheck")
+public class HealthCheckController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "OK";
+    }
+}
