b0a63979dd379a16d06f3fef58dc82f916bdc94a
geniuus <cross_man@naver.com>
2024-01-20 23:54:09 +0900

    FEAT/ List로 Message 보내는 기능 구현

--- ToMeetToMe/ToMeetToMe/src/main/java/com/example/tomeettome/Service/NotificationService.java
+++ ToMeetToMe/ToMeetToMe/src/main/java/com/example/tomeettome/Service/NotificationService.java
@@ -1,7 +1,10 @@
 package com.example.tomeettome.Service;
 
 import com.example.tomeettome.DTO.NotificationDTO;
+import com.example.tomeettome.Model.CalendarPermissionEntity;
 import com.example.tomeettome.Model.UserEntity;
+import com.example.tomeettome.Repository.CalendarPermissionRepository;
+import com.example.tomeettome.Repository.TeamRepository;
 import com.example.tomeettome.Repository.UserRepository;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
@@ -11,11 +14,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
 public class NotificationService {
+    @Autowired
+    private TeamRepository teamRepository;
+    @Autowired
+    private CalendarPermissionRepository calendarPermissionRepository;
 
     @Autowired private FirebaseMessaging firebaseMessaging;
     @Autowired UserRepository userRepository;
@@ -46,6 +54,42 @@ public class NotificationService {
         else return null;
     }
 
+    public List<NotificationDTO> makeModifyOrConfirmPromiseNotiDTOList(String teamIcsFileName, boolean flag) {
+        // 해당 팀의 permission들
+        List<CalendarPermissionEntity> permissions = calendarPermissionRepository.findByIcsFileName(teamIcsFileName);
+        List<String> fcmTokenList = new ArrayList<>();
+        List<NotificationDTO> result = new ArrayList<>();
+
+        // 팀에 해당하는 모든 유저들의 fcm토큰을 리스트에 넣고
+        for (CalendarPermissionEntity p : permissions) {
+            fcmTokenList.add(userRepository.findByUserId(p.getUserId()).getFcmToken());
+        }
+
+        // 알림 body에 넣을 팀 이름 찾고
+        String teamName = teamRepository.findByOriginKey(permissions.get(0).getOwnerOriginKey()).getName();
+
+        String title = "";
+        String body = "";
+        if (flag) {
+            title = "약속 수정";
+            body = "팀 " + teamName + "의 약속이 수정되었습니다. 확인해주세요 !";
+        }
+        else {
+            title = "약속 확정";
+            body = "팀 " + teamName + "의 약속이 확정되었습니다. 확인해주세요 !";
+        }
+
+        for (String tklist : fcmTokenList) {
+            NotificationDTO noti = NotificationDTO.builder()
+                    .fcmToken(tklist)
+                    .title(title)
+                    .body(body)
+                    .build();
+            result.add(noti);
+        }
+        return result;
+    }
+
     public Message makeMessageByToken(NotificationDTO dto) {
         UserEntity user = userRepository.findByFcmToken(dto.getFcmToken());
 
@@ -72,6 +116,30 @@ public class NotificationService {
             log.error("Not Exist This User" + dto.getFcmToken());
             return null;
         }
+    }
+
+    public List<Message> makeMessagesByToken(List<NotificationDTO> dtos) {
+        List<Message> msgList = new ArrayList<>();
+
+        for (NotificationDTO noti : dtos) {
+            if (noti.getFcmToken() != null) {
+                Notification notification = Notification.builder()
+                        .setTitle(noti.getTitle())
+                        .setBody(noti.getBody())
+                        .build();
+
+                Message message = Message.builder()
+                        .setToken(noti.getFcmToken())
+                        .setNotification(notification)
+                        .build();
+                msgList.add(message);
+            }
+            else {
+                log.error("Not Exist This User's FCM Token" + noti.getFcmToken());
+                return null;
+            }
+        }
+        return msgList;
     }
 
     public void sendNotificaton(Message message) {
