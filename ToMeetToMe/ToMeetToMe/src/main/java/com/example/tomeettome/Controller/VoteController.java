6800d0a5f4ecdba08df3d55dfd2410f4e9eac972
geniuus <cross_man@naver.com>
2024-02-04 15:47:21 +0900

    CLN/ Vote Controlloer 에러처리 구문 추가

--- ToMeetToMe/ToMeetToMe/src/main/java/com/example/tomeettome/Controller/VoteController.java
+++ ToMeetToMe/ToMeetToMe/src/main/java/com/example/tomeettome/Controller/VoteController.java
@@ -22,29 +22,43 @@ import java.util.List;
 public class VoteController {
     @Autowired VoteService voteService;
 
-
     @PutMapping("/like")
     public ResponseEntity<?> like(@AuthenticationPrincipal String userId,
                                   @RequestBody VoteDTO dto) {
-        voteService.like(userId, VoteDTO.toEntity(dto));
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        try {
+            voteService.like(userId, VoteDTO.toEntity(dto));
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
 
     @PutMapping("/dislike")
     public ResponseEntity<?> dislike(@AuthenticationPrincipal String userId,
                                      @RequestBody VoteDTO dto) {
-        voteService.dislike(userId, VoteDTO.toEntity(dto));
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        try {
+            voteService.dislike(userId, VoteDTO.toEntity(dto));
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @GetMapping("/retrieve/{promiseUid}")
     public ResponseEntity<?> retrieveLikes(@AuthenticationPrincipal String userId,
                                            @PathVariable("promiseUid") String promiseUid) {
-        List<VoteEntity> votes = voteService.retreive(promiseUid, userId);
+        try {
+            List<VoteEntity> votes = voteService.retreive(promiseUid, userId);
 
-        ResponseDTO response = ResponseDTO.<VoteEntity>builder().data(votes).status("succeed").build();
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+            ResponseDTO response = ResponseDTO.<VoteEntity>builder().data(votes).status("succeed").build();
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
 }
